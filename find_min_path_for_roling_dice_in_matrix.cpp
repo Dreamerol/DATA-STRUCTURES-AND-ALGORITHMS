6f6b5fb0d0eedd3a761b8c48f7c7e2ea81faa95a
Dreamerol <108183728+Dreamerol@users.noreply.github.com>
2025-12-04 16:13:18 +0200

    Update find_min_path_for_roling_dice_in_matrix.cpp

--- find_min_path_for_roling_dice_in_matrix.cpp
+++ find_min_path_for_roling_dice_in_matrix.cpp
@@ -58,3 +58,75 @@ int main()
     cout<<ways(dp, dice, n, m ,0, 0);
     return 0;
 }
+
+
+
+//void rotateDiceBackward(vector<int>& dice){
+//      dice = {dice[1], dice[5], dice[2], dice[0], dice[4],dice[3]};
+    
+// }
+// void rotateDiceLeft(vector<int>& dice){
+//     dice = {dice[2], dice[1], dice[5], dice[3], dice[0],dice[4]};
+      
+// }
+
+
+// int ways(vector<vector<int>>& dp, vector<int> dice, int i, int j){
+//     if(i<0 || j<0) return 1e9;
+//     if(i == 0 && j == 0) return dp[0][0] = dice[5];
+    
+//     if(dp[i][j] != -1) return dp[i][j];
+//     if(i == 0){
+//         rotateDiceLeft(dice);
+//         return dp[i][j] = ways(dp, dice,i,j-1) + dice[5];
+//     }
+//     if(j == 0){
+//         rotateDiceBackward(dice);
+//         return dp[i][j] = ways(dp, dice, i-1, j) + dice[5];
+//     }
+//     rotateDiceLeft(dice);
+//     int k1 = ways(dp, dice,i, j-1) + dice[5];
+//     rotateDiceBackward(dice);
+//     int k2 = ways(dp, dice, i-1, j) + dice[5];
+    
+//     return dp[i][j] = min(k1, k2);
+    
+// }
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+const int INF = 1e9;
+void rotateDiceBackward(vector<int>& dice){
+     dice = {dice[1], dice[5], dice[2], dice[0], dice[4],dice[3]};
+    
+}
+void rotateDiceLeft(vector<int>& dice){
+    dice = {dice[2], dice[1], dice[5], dice[3], dice[0],dice[4]};
+      
+}
+int ways(int i, int j, vector<int>& dice){
+    if(i < 0 || j < 0)return INF;
+    if(i == 0 && j == 0)return dice[5];
+    vector<int> L = dice;
+    rotateDiceLeft(L);
+    int cost1 = ways(i, j-1, L) + dice[5];
+    vector<int> B = dice;
+    rotateDiceBackward(B);
+    int cost2 = ways(i-1,j,B) + dice[5];
+
+return min(cost1, cost2);
+    
+}
+int main()
+ {
+     
+    vector<int> dice = {6,2,5,3,4,1};
+    int n = 2;
+    int m = 2;
+ 
+    cout<<ways(n-1, m-1, dice);
+    return 0;
+}
+
